6c813fefc1c6bcb7e1c1db507402e6e6f5f4b5ac
Milan Šobot <sobotmilan3@gmail.com>
2026-08-19 22:56:57 +0200

    pom.xml and Main.java revisions

--- src/org/unibl/etf/pj2/luka/main/Main.java
+++ src/org/unibl/etf/pj2/luka/main/Main.java
@@ -1,20 +1,25 @@
 package org.unibl.etf.pj2.luka.main;
+
 import org.unibl.etf.pj2.luka.gui.AdminProzor;
+
 import javax.swing.SwingUtilities;
+
 /**
- * Ulazna tačka čitave aplikacije
+ * Ulazna tačka čitave aplikacije.
  *
- * <p>Iako je administratorska GUI aplikacija tehnički stvarna ulazna tačka,
- * za svrhu jednostavnosti i jasnoće kreacija Admin GUI prozora je izvršena iz main() metode.</p>
- *
+ * <p>Podrazumijevano se pokreće administratorski
+ * dio aplikacije, sa dugmetom prema klijentskom dijelu.</p>
  *
  * @author Milan Šobot
  * @version 1.0
  */
-
-
 public class Main {
 
+    /**
+     * Pokreće administratorsku aplikaciju na Swing niti za obradu događaja.
+     *
+     * @param args Argumenti komandne linije; ne koriste se.
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new AdminProzor().setVisible(true));
     }
